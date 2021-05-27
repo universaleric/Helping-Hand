f3fb69bf2379fcbcb1cf975349da9a2e2e2b6610
Eric Hay <ericdanielhay@yahoo.com>
2021-05-26 21:52:44 -0500

    Associated user_id with skills_id for userskill table.

--- controllers/api/userSkillRoutes.js
+++ controllers/api/userSkillRoutes.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
 const { Userskill } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-
-router.post("/", async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
-    const userskillData = await Userskill.create(req.body);
+    const userskillData = await Userskill.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
     console.log(userskillData);
 
-    
     res.status(200).json(userskillData);
-    
   } catch (err) {
     res.status(400).json(err);
   }
 });
-
 
 module.exports = router;
